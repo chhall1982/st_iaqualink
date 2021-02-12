5a74430a3e4fbc9a58735918cc0da9d60e059efc
Chris Hall <chris.hall@atmosenergy.com>
2021-02-12 16:16:29 -0600

    outline

---
+++ st_iaqualink.src/st_iaqualink.groovy
@@ -0,0 +1,26 @@
+/**
+ *  iAqualink
+ *
+ *  Copyright 2018 Chris Hall
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License..
+ *
+ */
+
+ definition(
+    name: "iAquaLink",
+    namespace: "chhall1982",
+    author: "Chris Hall",
+    description: "Integrate iAquaLink Smart Pool Controller with Smartthings",
+    category: "SmartThings Labs",
+    iconUrl: "https://raw.githubusercontent.com/chhall1982/st_iaqualink/master/icons/iaq.png",
+    iconX2Url: "https://raw.githubusercontent.com/chhall1982/st_iaqualink/master/icons/iaq@2x.png",
+    iconX3Url: "https://raw.githubusercontent.com/chhall1982/st_iaqualink/master/icons/iaq@3x.png",
+ )
